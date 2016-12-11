d612bcb66fe5795e495a6a54abefc46316cbb253
FazilFahadSA <fahad.frontend.developer@gmail.com>
2016-12-11 08:32:02 +0530

    Content section completed
    Left sidebar, header and content section completed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-jQuery(document).ready(function (e) {
-  console.log("ready document !")
+jQuery(document).ready(function(e) {
     function t(t) {
-        e(t).bind("click", function (t) {
+        e(t).bind("click", function(t) {
             t.preventDefault();
             e(this).parent().fadeOut()
         })
     }
-    e(".dropdown-toggle").click(function () {
+    e(".dropdown-toggle").click(function() {
         var t = e(this).parents(".button-dropdown").children(".dropdown-menu").is(":hidden");
         e(".button-dropdown .dropdown-menu").hide();
         e(".button-dropdown .dropdown-toggle").removeClass("active");
@@ -14,15 +13,39 @@ jQuery(document).ready(function (e) {
             e(this).parents(".button-dropdown").children(".dropdown-menu").toggle().parents(".button-dropdown").children(".dropdown-toggle").addClass("active")
         }
     });
-    e(document).bind("click", function (t) {
+    e(document).bind("click", function(t) {
         var n = e(t.target);
         if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-menu").hide();
     });
-    e(document).bind("click", function (t) {
+    e(document).bind("click", function(t) {
         var n = e(t.target);
         if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-toggle").removeClass("active");
     })
-  jQuery(window).resize(function() {
-    // sidebarWidthControl();
-  });
+
+    function preventBodyScroll() {
+        $("aside").mouseenter(function() {
+            $("body").addClass("prevent-scroll");
+        });
+        $("aside").mouseleave(function() {
+            $("body").removeClass("prevent-scroll");
+        });
+    }
+    preventBodyScroll();
+
+    function toggleMobile() {
+        $(".toggle-btn").click(function() {
+            $(".toggle-btn, aside").toggleClass("active");
+            /* Check panel overlay */
+            // if ($(".panel-overlay").hasClass("active")) {
+            //     $(".panel-overlay").fadeIn();
+            // } else {
+            //     $(".panel-overlay").fadeOut();
+            // }
+        });
+    }
+    toggleMobile();
+
+    jQuery(window).resize(function() {
+        // sidebarWidthControl();
+    });
 });
